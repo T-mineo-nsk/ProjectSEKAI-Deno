cf6fc099f334ad86a272e6b8518b1c9b01d79cd1
峯尾 武瑠 <t.mineo@nagoya.nskint.co.jp>
2025-05-22 18:00:00 +0900

    Initial commit for Deno bot

---
+++ utils/createCommand.ts
@@ -0,0 +1,7 @@
+// utils/createCommand.ts
+import { SlashCommandPartial } from "../deps.ts";
+
+// この関数はコマンド定義をそのまま返すだけです
+export function createCommand(command: SlashCommandPartial) {
+  return command;
+}
