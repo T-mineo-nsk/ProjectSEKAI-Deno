31945426700d64b5959308258ecb059808649824
峯尾 武瑠 <t.mineo@nagoya.nskint.co.jp>
2025-05-22 14:13:19 +0900

    Initial commit for Deno bot

--- loadCommands.ts
+++ loadCommands.ts
@@ -1,12 +1,9 @@
 // utils/loadCommands.ts
 import { Collection } from "https://esm.sh/discord.js@14.13.0";
 
-// 必要に応じてコマンドをここで直接インポートする形式に変えます
-import { eventPointCommand } from "../commands/eventpoint.ts";
-import { skillRateCommand } from "../commands/skillrate.ts";
+import eventpoint from "./commands/eventpoint.ts";
+import skillrate from "./commands/skillrate.ts";
 
-export async function loadCommands(client: any) {
-  client.commands = new Collection();
-  client.commands.set(eventPointCommand.data.name, eventPointCommand);
-  client.commands.set(skillRateCommand.data.name, skillRateCommand);
+export function loadCommands() {
+  return [eventpoint, skillrate];
 }
